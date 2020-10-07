653428c181434719f71991acc57f67bbc12f3141
Faizee <faiyazsiddique9@gmail.com>
2020-10-07 10:28:24 +0530

    Proper Commenting

--- trainner.py
+++ trainner.py
@@ -2,17 +2,17 @@ import cv2,os
 import numpy as np
 from PIL import Image
 
-recognizer = cv2.createLBPHFaceRecognizer()
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 def getImagesAndLabels(path):
     #get the path of all the files in the folder
     imagePaths=[os.path.join(path,f) for f in os.listdir(path)] 
-    #create empth face list
+    #create empty face list
     faceSamples=[]
     #create empty ID list
     Ids=[]
-    #now looping through all the image paths and loading the Ids and the images
+    # looping through all the image paths 
     for imagePath in imagePaths:
         #loading the image and converting it to gray scale
         pilImage=Image.open(imagePath).convert('L')
